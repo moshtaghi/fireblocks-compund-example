b3d476d312549bee1c24f6787efa4990ce357df8
Mehdi Moshtaghi <mehdi.mm@gmail.com>
2021-06-21 19:22:03 +0430

    Add exit market

---
+++ src/exitMarkets.ts
@@ -0,0 +1,19 @@
+import { comptrollerContract } from './assets/contracts';
+import contractAddress from './assets/contractAddress';
+import { SendToFireBlocks } from './assets/fireblocks';
+
+(async function () {
+  if (process.argv.length < 3) {
+    console.error('Usage: node dist/exitMarkets.js <YOUR VAULT ID>');
+    process.exit(1);
+  }
+
+  let tx = await comptrollerContract.populateTransaction.exitMarket(
+    contractAddress.Tokens.cETH.address
+  );
+
+  await SendToFireBlocks(process.argv[2], tx);
+})().catch((err) => {
+  console.log('======> error', err);
+  process.exit(1);
+});
