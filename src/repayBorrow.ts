5a08f5a1249946eead36df7f0024003e63064079
Mehdi Moshtaghi <mehdi.mm@gmail.com>
2021-06-21 19:21:52 +0430

    Add repay borrow

---
+++ src/repayBorrow.ts
@@ -0,0 +1,32 @@
+import { cUSDTcontract, USDTcontract } from './assets/contracts';
+import contractAddress from './assets/contractAddress';
+import { SendToFireBlocks } from './assets/fireblocks';
+
+(async function () {
+  if (process.argv.length < 4) {
+    console.error(
+      'Usage: node dist/repayBorrow.js <YOUR VAULT ID> <YUR KOVAN ETH ADDRESS>'
+    );
+    process.exit(1);
+  }
+  console.log('getting borrowed balance...');
+  let cUSDT = await cUSDTcontract.callStatic.borrowBalanceCurrent(
+    process.argv[3]
+  );
+  console.log(`you borrowed ${cUSDT / 1e6} USDT`);
+  console.log('repaying the borrow...');
+  console.log('approving...');
+  const underlyingToRepay = ((cUSDT / 1e6) * Math.pow(10, 6)).toString();
+  const approveTx = await USDTcontract.populateTransaction.approve(
+    contractAddress.Contracts.cUSDT,
+    underlyingToRepay
+  );
+  await SendToFireBlocks(process.argv[2], approveTx);
+  let tx = await cUSDTcontract.populateTransaction.repayBorrow(
+    underlyingToRepay
+  );
+  await SendToFireBlocks(process.argv[2], tx);
+})().catch((err) => {
+  console.log('error', err);
+  process.exit(1);
+});
